cb150ce326077a2a7e3341cbe1ae2eacac468e11
Brian Meek <brian@hntlabs.com>
2022-07-06 16:02:50 -0700

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import * as styles from "./styles.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <header className={styles.menu}>
+        <img src={logo} className={styles.menuItem} alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className={styles.menuItem}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className={styles.menuItem}
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
